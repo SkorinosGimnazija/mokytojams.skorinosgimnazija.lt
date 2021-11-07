a298da6f3624c630b47f6242e2bc949991de49ef
eXmaS <76765030+eXmaS@users.noreply.github.com>
2021-11-07 13:19:49 +0200

    up

--- components/buttons/GoogleLogInButton.tsx
+++ components/buttons/GoogleLogInButton.tsx
@@ -10,7 +10,7 @@ export const GoogleLogInButton = () => {
       onClick={() => auth.logIn()}
       className="inline-flex items-center gap-2 py-2 pl-2 pr-5 font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent hover:bg-blue-50"
     >
-      <img src={GoogleLogo} height={40} width={40} alt="Google" />
+      <img src={GoogleLogo.src} width={40} alt="Google" />
       <span>Prisijungti</span>
     </button>
   );
