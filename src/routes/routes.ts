b8ebd330f769c8339377f6ac837652f6a9038c70
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-06-02 23:28:10 +0300

    temp hide reg

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -162,7 +162,7 @@ export const routes: RoutesGroup[] = [
       {
         name: 'Metiniai pokalbiai',
         path: '/teacher/appointments/yearly',
-        accessRole: 'Teacher',
+        accessRole: 'Admin',
         innerRoutes: [
           {
             path: '',
