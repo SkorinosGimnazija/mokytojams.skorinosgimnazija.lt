52fb3f33c0a1be6e428ea0e3c104a6ef3de9fcb2
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-06-25 13:31:10 +0300

    round courses price/hours

--- src/components/lists/CourseStatsList.tsx
+++ src/components/lists/CourseStatsList.tsx
@@ -4,6 +4,7 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { Box } from '@mui/system';
+import round from 'lodash/round';
 import React, { useMemo, useState } from 'react';
 import { toLocalDate } from '../../lib/dateFormat';
 import { CourseStatsDto } from '../../services/generatedApi';
@@ -19,7 +20,7 @@ export const CourseStatsList: React.FC<Props> = ({ coursesData, dateRange, ...pr
   const [expanded, setExpanded] = useState<Record<string, boolean | undefined>>({});
 
   const totals = useMemo(() => {
-    return (coursesData ?? []).reduce(
+    const sum = (coursesData ?? []).reduce(
       (acc, cur) => {
         return {
           hours: acc.hours + cur.hours,
@@ -30,6 +31,11 @@ export const CourseStatsList: React.FC<Props> = ({ coursesData, dateRange, ...pr
       },
       { price: 0, hours: 0, count: 0, usefulCount: 0 }
     );
+
+    sum.hours = round(sum.hours, 2);
+    sum.price = round(sum.price, 2);
+
+    return sum;
   }, [coursesData]);
 
   const handleExpand = (userId: number) => {
@@ -37,84 +43,82 @@ export const CourseStatsList: React.FC<Props> = ({ coursesData, dateRange, ...pr
   };
 
   return (
-    <>
-      <DefaultTable {...props}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Mokytojų kursai</TableCell>
-            <TableCell width="150px" align="center">
-              Kiekis
-            </TableCell>
-            <TableCell width="150px" align="center">
-              Kaina
-            </TableCell>
-            <TableCell width="150px" align="center">
-              Trukmė
-            </TableCell>
-            <TableCell width="150px" align="center">
-              Atnaujinta
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {coursesData?.map((teacher) => {
-            return (
-              <React.Fragment key={teacher.userId}>
-                <TableRow hover sx={{ cursor: 'pointer' }} onClick={() => handleExpand(teacher.userId)}>
-                  <TableCell>
-                    <Typography>{teacher.userDisplayName}</Typography>
-                  </TableCell>
+    <DefaultTable {...props}>
+      <TableHead>
+        <TableRow>
+          <TableCell>Mokytojų kursai</TableCell>
+          <TableCell width="150px" align="center">
+            Kiekis
+          </TableCell>
+          <TableCell width="150px" align="center">
+            Kaina
+          </TableCell>
+          <TableCell width="150px" align="center">
+            Trukmė
+          </TableCell>
+          <TableCell width="150px" align="center">
+            Atnaujinta
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {coursesData?.map((teacher) => {
+          return (
+            <React.Fragment key={teacher.userId}>
+              <TableRow hover sx={{ cursor: 'pointer' }} onClick={() => handleExpand(teacher.userId)}>
+                <TableCell>
+                  <Typography>{teacher.userDisplayName}</Typography>
+                </TableCell>
 
-                  <TableCell align="center">
-                    <Typography>
-                      {teacher.count} ({teacher.usefulCount})
-                    </Typography>
-                  </TableCell>
+                <TableCell align="center">
+                  <Typography>
+                    {teacher.count} ({teacher.usefulCount})
+                  </Typography>
+                </TableCell>
 
-                  <TableCell align="center">
-                    <Typography>{teacher.price} €</Typography>
-                  </TableCell>
+                <TableCell align="center">
+                  <Typography>{teacher.price} €</Typography>
+                </TableCell>
 
-                  <TableCell align="center">
-                    <Typography>{teacher.hours} val.</Typography>
-                  </TableCell>
+                <TableCell align="center">
+                  <Typography>{teacher.hours} val.</Typography>
+                </TableCell>
 
-                  <TableCell align="center">
-                    <Typography>{toLocalDate(teacher.lastUpdate)}</Typography>
+                <TableCell align="center">
+                  <Typography>{toLocalDate(teacher.lastUpdate)}</Typography>
+                </TableCell>
+              </TableRow>
+              {expanded[teacher.userId] && (
+                <TableRow>
+                  <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={5}>
+                    <Box sx={{ margin: 2 }}>
+                      <CoursesAdminList dateRange={dateRange} teacherId={teacher.userId} />
+                    </Box>
                   </TableCell>
                 </TableRow>
-                {expanded[teacher.userId] && (
-                  <TableRow>
-                    <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={5}>
-                      <Box sx={{ margin: 2 }}>
-                        <CoursesAdminList dateRange={dateRange} teacherId={teacher.userId} />
-                      </Box>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </React.Fragment>
-            );
-          })}
-        </TableBody>
-        <TableFooter>
-          <TableRow>
-            <TableCell align="right">
-              <Typography>Iš viso:</Typography>
-            </TableCell>
-            <TableCell align="center">
-              <Typography>
-                {totals.count} ({totals.usefulCount})
-              </Typography>
-            </TableCell>
-            <TableCell align="center">
-              <Typography>{totals.price} €</Typography>
-            </TableCell>
-            <TableCell align="center">
-              <Typography>{totals.hours} val.</Typography>
-            </TableCell>
-          </TableRow>
-        </TableFooter>
-      </DefaultTable>
-    </>
+              )}
+            </React.Fragment>
+          );
+        })}
+      </TableBody>
+      <TableFooter>
+        <TableRow>
+          <TableCell align="right">
+            <Typography>Iš viso:</Typography>
+          </TableCell>
+          <TableCell align="center">
+            <Typography>
+              {totals.count} ({totals.usefulCount})
+            </Typography>
+          </TableCell>
+          <TableCell align="center">
+            <Typography>{totals.price} €</Typography>
+          </TableCell>
+          <TableCell align="center">
+            <Typography>{totals.hours} val.</Typography>
+          </TableCell>
+        </TableRow>
+      </TableFooter>
+    </DefaultTable>
   );
 };
