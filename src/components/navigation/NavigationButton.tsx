61b21f8319d95d9c5378c852560a7807bb689f9e
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-06-25 08:14:08 +0300

    highlight menu item on route match

--- src/components/navigation/NavigationButton.tsx
+++ src/components/navigation/NavigationButton.tsx
@@ -3,7 +3,7 @@ import { ListItemButton } from '@mui/material';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useMatch } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { hideDrawer } from '../../store/drawerSlice';
 
@@ -14,23 +14,23 @@ interface Props {
 
 export const NavigationButton: React.FC<Props> = ({ name, slug }) => {
   const dispatch = useAppDispatch();
+  const routeMatch = useMatch({ path: slug, end: false });
 
   return (
-    <>
-      <ListItemButton
-        dense
-        disableRipple
-        disableTouchRipple
-        component={Link}
-        to={slug}
-        sx={{ marginX: 2, borderRadius: 2 }}
-        onClick={() => dispatch(hideDrawer())}
-      >
-        <ListItemIcon sx={{ minWidth: '2rem' }}>
-          <ArrowForwardIosOutlinedIcon color="primary" fontSize="small" />
-        </ListItemIcon>
-        <ListItemText primary={name} />
-      </ListItemButton>
-    </>
+    <ListItemButton
+      dense
+      disableRipple
+      disableTouchRipple
+      selected={Boolean(routeMatch)}
+      component={Link}
+      to={slug}
+      sx={{ marginX: 2, borderRadius: 2 }}
+      onClick={() => dispatch(hideDrawer())}
+    >
+      <ListItemIcon sx={{ minWidth: '2rem' }}>
+        <ArrowForwardIosOutlinedIcon color="primary" fontSize="small" />
+      </ListItemIcon>
+      <ListItemText primary={name} />
+    </ListItemButton>
   );
 };
