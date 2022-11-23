95ea52210cb05a19bf45bf5237d14f9cae3cc07a
eXmaS <76765030+eXmaS@users.noreply.github.com>
2022-11-23 08:26:54 +0200

    notes link

--- src/components/icons/StatusIcon.tsx
+++ src/components/icons/StatusIcon.tsx
@@ -1,7 +1,7 @@
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 import CheckCircleOutlinedIcon from '@mui/icons-material/CheckCircleOutlined';
 import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined';
-import { Tooltip } from '@mui/material';
+import { Link, Tooltip } from '@mui/material';
 import React from 'react';
 
 interface Props {
@@ -9,13 +9,35 @@ interface Props {
   notes?: string | null;
 }
 
+const tooltipTitle = (notes?: string | null) => {
+  if (notes == null || notes.length === 0) {
+    return null;
+  }
+
+  if (notes.startsWith('http')) {
+    return (
+      <Link
+        href={notes}
+        color="inherit"
+        target="_blank"
+        rel="noreferrer noopener"
+        onClick={(e) => e.stopPropagation()}
+      >
+        {notes}
+      </Link>
+    );
+  }
+
+  return notes;
+};
+
 export const StatusIcon: React.FC<Props> = ({ status, notes }) => {
   if (status == null) {
     return null;
   }
 
   return (
-    <Tooltip title={notes}>
+    <Tooltip title={tooltipTitle(notes)}>
       <span>
         {status ? <CheckCircleOutlinedIcon color="success" /> : <CancelOutlinedIcon color="error" />}
         {notes && <ErrorOutlineOutlinedIcon color="warning" />}
